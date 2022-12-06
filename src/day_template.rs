5b84e56f0411057b7cd4951e9eee39b5bb209619
Elias F. Fyksen <git@fyksen.net>
2022-12-06 22:08:19 +0000

    Improved setup

---
+++ src/day_template.rs
@@ -0,0 +1,10 @@
+
+pub const DATA_PATH: &str = "data/day1.txt";
+
+pub fn p1(input: String) -> usize {
+  unimplemented!()
+}
+
+pub fn p2(input: String) -> usize {
+  unimplemented!()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
 #![feature(iter_array_chunks)]
 
-use std::{fs::read_to_string,};
+use std::fs::read_to_string;
 
 pub mod day5;
-
+use day5 as day;
 
 fn main() {
-    println!("{:?}", day5::p2(
-        read_to_string("./data/day5.txt").unwrap()
-    ));
+    let input_test = "    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2".to_string();
+
+    let input_real = read_to_string(day::DATA_PATH).unwrap();
+
+    println!("PART 1:");
+    println!("\tTEST: {:?}", day::p1(input_test.clone()));
+    println!("\tREAL: {:?}", day::p1(input_real.clone()));
+
+    println!("PART 2:");
+    println!("\tTEST: {:?}", day::p2(input_test.clone()));
+    println!("\tREAL: {:?}", day::p2(input_real.clone()));
 }

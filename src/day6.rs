0a7af65efa1bcd3f593defac228d9cfed4ea56d7
Elias F. Fyksen <git@fyksen.net>
2022-12-06 07:57:10 +0000

    Add day 6

---
+++ src/day6.rs
@@ -0,0 +1,52 @@
+
+pub fn p1(input: String) -> i32 {
+  let mut dups = 0;
+  let mut buffer = String::new();
+
+  for (i, c) in input.chars().enumerate() {
+    if buffer.find(c).is_some() {
+      dups += 1;
+    }
+    buffer.push(c);
+
+    if buffer.len() > 4 {
+      let c = buffer.remove(0);
+      if buffer.find(c).is_some() {
+        dups -= 1;
+      }
+    }
+
+    if buffer.len() == 4 {
+      if dups == 0 {
+        return i as i32 + 1;
+      }
+    }
+  }
+  panic!()
+}
+
+pub fn p2(input: String) -> i32 {
+  let mut dups = 0;
+  let mut buffer = String::new();
+
+  for (i, c) in input.chars().enumerate() {
+    if buffer.find(c).is_some() {
+      dups += 1;
+    }
+    buffer.push(c);
+
+    if buffer.len() > 14 {
+      let c = buffer.remove(0);
+      if buffer.find(c).is_some() {
+        dups -= 1;
+      }
+    }
+
+    if buffer.len() == 14 {
+      if dups == 0 {
+        return i as i32 + 1;
+      }
+    }
+  }
+  panic!()
+}

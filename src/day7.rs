d2474b13f68e6b4c6829619f306ac5fa1a999415
Elias F. Fyksen <31504893+eliasffyksen@users.noreply.github.com>
2022-12-08 17:05:52 +0000

    Dear Day 7, I take back what I said, you are beautiful

--- src/day7.rs
+++ src/day7.rs
@@ -1,64 +1,77 @@
-use std::{collections::HashMap, rc::Rc, cell::RefCell, borrow::BorrowMut};
-
+use std::collections::HashMap;
 use regex::Regex;
-
 
 pub const DATA_PATH: &str = "data/day7.txt";
 
-type Directory = Rc<RefCell<HashMap<String, Entry>>>;
+#[derive(Debug, Default)]
+struct Node {
+    size: usize,
+    children: HashMap<String, Node>
+}
 
-enum Entry {
-  File(usize),
-  Dir(Directory),
+fn enter<'a>(dir: &mut Node, lines: &mut impl Iterator<Item = &'a str>) -> bool {
+    let re_cd = Regex::new(r"^\$ cd (.+)").unwrap();
+    let re_file = Regex::new(r"^(\d+) (.+)").unwrap();
+
+    let mut next = lines.next();
+    let mut exit_all = false;
+
+    while let Some(line) = next {
+        if let Some(cap) = re_cd.captures(line) {
+            let dir_name = cap.get(1).unwrap().as_str();
+
+            if dir_name == ".." {
+                break;
+            } else if dir_name == "/" || enter(dir.children.entry(dir_name.to_string()).or_default(), lines)
+            {
+                exit_all = true;
+                break;
+            }
+
+        } else if let Some(cap) = re_file.captures(line) {
+            let size = cap.get(1).unwrap().as_str().parse::<usize>().unwrap();
+            let file_name = cap.get(2).unwrap().as_str();
+            dir.children.entry(file_name.to_string()).or_default().size = size;
+        }
+        next = lines.next();
+    }
+
+    dir.size = dir.children.iter().map(|(_, v)| v.size).sum();
+
+    exit_all
+}
+
+fn parse(input: String) -> Node {
+    let mut reader = input.trim().lines().peekable();
+    let mut tree: Node = Default::default();
+
+    while let Some(_) = reader.peek() {
+        enter(&mut tree, &mut reader);
+    }
+
+    tree
 }
 
 pub fn p1(input: String) -> usize {
-  let re_cd = Regex::new(r"$ cd (.*)").unwrap();
-  let re_ls = Regex::new(r"$ ls").unwrap();
-  let re_file = Regex::new(r"(\d+) (.*)").unwrap();
+    parse(input).size
+}
 
-  let root: Directory = Default::default();
-
-  let mut path = vec![];
-
-  let mut input = input.trim().lines();
-
-  for line in input {
-    if let Some(cap) = re_cd.captures(line) {
-      let dir = cap.get(1).unwrap().as_str();
-      println!("Entring {:?}", dir);
-
-      if dir == "/" {
-        path = vec![];
-      } if dir == ".." {
-        path.pop();
-      } else {
-        path.push(dir.to_string());
-      }
-
-    } else if let Some(cap) = re_file.captures(line) {
-      let size = cap.get(1).unwrap().as_str().parse::<usize>().unwrap();
-      let name = cap.get(1).unwrap().as_str();
-      println!("FILE: {:?} {name} {size}", path);
-
-      let mut path = path.clone();
-      let mut current = root.as_ref();
-
-      while path.len() > 0 {
-        let key = path.remove(0);
-        
-        if let Entry::Dir(dir) = current.entry(key).or_insert(Entry::Dir(Default::default())) {
-          tmp = dir.borrow_mut();
-
-        } else {
-          panic!("Not a directory");
-        }
-      }
+fn find_space(tree: &Node, target_size: usize) -> usize {
+    if tree.children.len() == 0 {
+        return usize::MAX;
     }
-  }
-  unimplemented!()
+
+    tree.children.iter()
+        .map(|(_, node)| find_space(node, target_size))
+        .chain(vec![tree.size,usize::MAX])
+        .filter(|v| *v > target_size)
+        .min().unwrap()
 }
 
 pub fn p2(input: String) -> usize {
-  unimplemented!()
+    let tree = parse(input);
+    let unused_space = 70000000 - tree.size;
+    let space_to_free = 30000000 - unused_space;
+
+    find_space(&tree, space_to_free)
 }
